73df1887645b39956b0723861366c0ef379926c5
Mohd Shuaib <shoebsiddiqui4947@gmail.com>
2024-08-18 05:55:11 +0530

    .

--- src/constants/backend.js
+++ src/constants/backend.js
@@ -1,1 +1,2 @@
-export const DB_URL = "https://profile-backend-bwxw.onrender.com";
+// export const DB_URL = "https://profile-backend-bwxw.onrender.com";
+export const DB_URL = "http://localhost:4000";
